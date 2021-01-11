8d61b5ef1e1c8ec0df2d0a1100ff14122d09b04e
Linerre <leon.errelin@outlook.com>
2021-01-11 21:27:00 +0800

    modified

--- wechat/assets/tny/header.py
+++ wechat/assets/tny/header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # get tny header part of personal history articel page
+# skip header img which is better to be extracted with other cartoons
 
 
 from urllib.request import urlopen
@@ -16,33 +17,35 @@ class GetHeader(HTMLParser):
         # column and issue number
         if tag == 'a' and ('class' in attrs[0] and 'rubric__link' in attrs[0][1]):
             self.counter += 1
-        if self.counter and tag == 'span':
+        elif self.counter and tag == 'span':
             self.nested += 1
-        if tag == 'a':
-            print(attrs)
+        #if tag == 'a':
+        #    print(attrs)
 
         # headline
-        if tag == 'h1':
+        elif tag == 'h1':
             self.counter += 1
         # subheadline
-        if tag == 'div' and ('class', 'content-header__row content-header__dek') in attrs:
+        elif tag == 'div' and ('class', 'content-header__row content-header__dek') in attrs:
+            self.counter += 1
+        # online publication date
+        # guess: this appears as static data, appearing before the author was inserted by js
+        # though author's name ends up preceding the time
+        elif tag == 'time':
             self.counter += 1
         # byline name
         # somehow the class attr is not included in the scraped result
+        # and it appears *after* the time tag in the scraped result (but *before* if using inspector)
         # perhaps it was added by js when rendering the site page
-        if tag == 'a' and ('href' in attrs[0] and 'contributors' in attrs[0][1]):
+        elif tag == 'a' and ('href' in attrs[0] and 'contributors' in attrs[0][1]):
             self.counter += 1
-        # online publication date
-        if tag == 'time':
-            self.counter += 1
-
 
     def handle_data(self, data):
         # data that has to be extracted by using the parents tag 
         # in which the data nested in
         if self.counter and self.nested:
-            if 'personal_history' not in self.content:
-                self.content['personal_history'] = data
+            if 'column' not in self.content:
+                self.content['column'] = data
             elif 'issue' not in self.content:
                 self.content['issue'] = data
         # data that can be extracted directly from the tag containing it 
@@ -51,15 +54,15 @@ class GetHeader(HTMLParser):
                 self.content['h1'] = data
             elif 'h2' not in self.content:
                 self.content['h2'] = data
-            elif 'author' not in self.content:
-                self.content['author'] = data
             elif 'time' not in self.content:
                 self.content['time'] = data
+            elif 'author' not in self.content:
+                self.content['author'] = data
 
     def handle_endtag(self, tag):
         if self.counter and self.nested:
             self.nested -= 1
-        if self.counter and not self.nested:
+        elif self.counter and not self.nested:
             self.counter -= 1
 
     def GetContent(self):
