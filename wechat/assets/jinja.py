6e746506edf5c74a8a15681e268eb8be64ce2a6b
errelin <leon.errelin@outlook.com>
2021-01-16 23:07:52 +0800

    tested one feature of templates

--- wechat/assets/jinja.py
+++ wechat/assets/jinja.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from jinja2 import Environment, FileSystemLoader
+from sys import argv
 
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
@@ -10,9 +11,15 @@ print(env.list_templates(extensions=["html"]))
     #TODO
     # figure out how this 'parent' argument works
     # and let the lookup order goes deeper
+    # FileSystemLoader will also load templates in subdirectories
+    # but it recognizes those templates as they are: /path/to/template
+    # if parent='string' given, then it would be string/path/to/template
+    # which in turn leads TemplateNotFound error
+
+    # better to keep templates in the same folder at the same depth
 template = env.get_template('te-base.html')
 
-output = template.render()
+output = template.render(title='Tiktok')
 print('output is', output)
 with open('test.html', 'w', encoding='utf-8') as test:
     test.write(output)
