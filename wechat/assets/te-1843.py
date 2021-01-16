59a1c6ed3301d6ff1391c591f8d40e8c8d477395
Linerre <leon.errelin@outlook.com>
2021-01-16 16:06:49 +0800

    first attempt: test te scraper

--- wechat/assets/te-1843.py
+++ wechat/assets/te-1843.py
@@ -1,22 +1,56 @@
 #!/usr/bin/env python3
 
-# Use self-made pakcage to parse 1843 article pages:
+# Parse te 1843 article pages:
 # headlines, paragraphs, imgs
 
 from urllib.request import urlopen
-from te_1843 import img
+from te import te_parser as te
 from sys import argv
 import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+import os.path
+
+project_dir = 'C:\\Users\\zl37\\projects\\gitty\\wechat'
+article_url = argv[1]
+article_dir = os.path.join(project_dir, str(argv[2])) 
+article_til = argv[3]
 
 try:
-    url = urlopen(argv[1])
+    # prepare html to feed
+    url = urlopen(article_url)
     html = url.read().decode('UTF-8')
     url.close()
+    
 
-    test_parse = img.GetImage()
-    test_parse.feed(html)
-    test_parse.store_imgs('img')
+    # prepare the article parser
+    te_article_header = te.GetHeader()
+    te_article_body = te.GetBody()
+    te_article_img = te.GetImage()
+
+    # scrape needed parts 
+    te_article_header.feed(html)
+    te_article_body.feed(html)
+    te_article_img.feed(html)
+    
+    # create header template
+    # header
+    headers = te_article_header.process_header()  # a list
+    with open(article_dir + article_til + '\\header.html', 'w', encoding='utf-8') as h:
+        for header in headers:
+            h.write(header)
+
+
+    # body
+    paras =  te_article_body.process_data()  # a list
+    with open(article_dir + article_til + '\\body.html', 'w', encoding='utf-8') as b:
+        for para in paras:
+            b.write(para)
+
+    # img
+    te_article_img.download_imgs(article_dir+article_til+'\\img')
+
+
+
 except Exception as error:
     logger.exception(error)
