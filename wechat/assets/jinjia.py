ef91d2b19c72be787958637c7ec2b003f4ecd189
Linerre <leon.errelin@outlook.com>
2021-01-16 12:38:35 +0800

    got to know jinja2 loader behavior: temps in sub dir must have prefix

--- wechat/assets/jinjia.py
+++ wechat/assets/jinjia.py
@@ -1,14 +1,18 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from jinja2 import Environment, FileSystemLoader
 
-try:
-	file_loader = FileSystemLoader('templates/')
-	env = Environment(loader=file_loader)
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
 
-	template = env.get_template('te-1843-base.html')
+print(env.list_templates(extensions=["html"]))
+    #TODO
+    # figure out how this 'parent' argument works
+    # and let the lookup order goes deeper
+template = env.get_template('te-temps/te-1843-base.html')
 
-	output = template.render()
-	print(output)
-	with open('test.html', 'w', encoding='utf-8') as test:
-		test.write(output)
-except Exception as error:
-	print(error)
+output = template.render()
+print('output is', output)
+	#with open('test.html', 'w', encoding='utf-8') as test:
+	#	test.write(output)
